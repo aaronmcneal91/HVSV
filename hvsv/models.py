c92cf034fc15d38b9d08a7402ffeb884ce61aaeb
aaronmcneal91 <98606248+aaronmcneal91@users.noreply.github.com>
2022-12-07 23:53:50 -0500

    Added citizens to database + model

--- hvsv/models.py
+++ hvsv/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Citizen(models.Model):
+    name = models.CharField(max_length=255)
+    alter_ego = models.CharField(max_length=255)
+    power = models.CharField(max_length=255)
+    affiliation = models.CharField(max_length=10)
+  
+
+def __str__(self):
+    return f"{self.name}"
